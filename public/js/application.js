b14979ca3cc4d4b7d008d2b951987889c55ed4a1
mcarthurgill <mickgill2008@gmail.com>
2013-06-30 13:35:13 -0700

    first commit

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	$('.game_form').on('submit', function(e){
+		$('.game_form').hide();
+		e.preventDefault(); 
+		var route = $(this).attr('action');
+		// console.log($(this));
+		var request = $(this).serialize();
+		console.log(request);
+		$.ajax({
+			type: "POST",
+			url: route,
+			data: { user_guess: $(this).find('input').val() }
+		}).done(function(data){
+			var response = data.guess.correct.toString()
+			$('#success').html("Your answer was " + response);
+			$('#success').fadeIn();
+			$('form:nth-child(1)').slideUp().remove();
+			setTimeout(function(){$('#success').fadeOut()},1500);
+		});
+	});
 });
